e4ec47a59f4ca4ae15ac8f8913d3759f4dbc2295
ZeeshanML <z33shanlatif007@gmail.com>
2024-09-19 11:58:15 +0500

    changed app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def predict_datapoint():
         return render_template('home.html', results = prediction[0])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
